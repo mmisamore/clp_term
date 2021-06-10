022f263410c9f0464dc80e181b83c1bc4a65d0ec
Michael D. Misamore <m.misamore@gmail.com>
2021-06-10 17:02:46 -0400

    Fix pack definition

--- pack.pl
+++ pack.pl
@@ -2,7 +2,7 @@ name(clp_term).
 
 title('clp(Term): Constraint Logic Programming for Terms in their Standard Ordering').
 author('Michael D. Misamore', 'm.misamore@gmail.com').
-home('https://github.com/mmisamore/clp-term').
+home('https://github.com/mmisamore/clp_term').
 
 version('0.9').
 download('https://github.com/mmisamore/clp_term/archive/0.9.zip').
