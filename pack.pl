7f3057a73c1a6b3985fe729d20228743df41cdc4
Michael D. Misamore <m.misamore@gmail.com>
2021-06-10 16:51:54 -0400

    Bump version in pack file

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(clp_term).
 
-title('clp(Term): Constraint Logic Programming for Terms in their standard ordering').
+title('clp(Term): Constraint Logic Programming for Terms in their Standard Ordering').
 author('Michael D. Misamore', 'm.misamore@gmail.com').
 home('https://github.com/mmisamore/clp-term').
 
-version('1.0.0').
-download('https://github.com/mmisamore/clp_term/archive/0.1.0.zip').
+version('0.9').
+download('https://github.com/mmisamore/clp_term/archive/0.9.zip').
 
